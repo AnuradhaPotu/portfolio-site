29fea5ace7334e8c5fa00e3e68b6bdecc465a7fd
Anuradha P <anuradhap@Anuradhas-MacBook-Pro.local>
2020-12-01 22:26:51 -0600

    fixed navbar toggle

--- src/components/Navbar/mynavbar.js
+++ src/components/Navbar/mynavbar.js
@@ -1,8 +1,9 @@
 import {Navbar,Nav} from 'react-bootstrap'
-import React from 'react'
+import React,{useState} from 'react'
 import "./mynavbar.style.css";
 
 const MyNavBar = () => {
+  const [expanded, setExpanded] = useState(false);
     return(
         <div>
         <Navbar
@@ -10,6 +11,7 @@ const MyNavBar = () => {
           variant="dark"
           expand="md"
           className="animate-navbar nav-theme justify-content-between"
+          expanded={expanded}
         >
           <div>
             <Navbar.Brand href="#home">
@@ -17,14 +19,14 @@ const MyNavBar = () => {
             </Navbar.Brand>
           </div>
           <div>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={() => setExpanded(expanded ? false : "expanded")} />
             <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="mr-auto ">
-                <Nav.Link href="#home">Home</Nav.Link>
-                <Nav.Link href="#about">About</Nav.Link>
-                <Nav.Link href="#skills">Skills</Nav.Link>
-                 <Nav.Link href="#timeline">Experience</Nav.Link>
-                <Nav.Link href="#contact">Contact</Nav.Link>
+              <Nav className="mr-auto " >
+                <Nav.Link href="#home" onClick={() => setExpanded(false)}>Home</Nav.Link>
+                <Nav.Link href="#about" onClick={() => setExpanded(false)}>About</Nav.Link>
+                <Nav.Link href="#skills" onClick={() => setExpanded(false)}>Skills</Nav.Link>
+                 <Nav.Link href="#timeline" onClick={() => setExpanded(false)}>Experience</Nav.Link>
+                <Nav.Link href="#contact" onClick={() => setExpanded(false)}>Contact</Nav.Link>
               </Nav>
             </Navbar.Collapse>
           </div>
